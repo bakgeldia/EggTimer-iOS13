2f5332faa4bfed3200e9b5b31d39030bc510cdb4
Bakgeldi Alkhabay <bakgeldia@gmail.com>
2024-09-22 15:11:20 +0500

    iOS App Development Bootcamp | Section 8 | EggTimer App - Added timer that counts down in seconds and changes label on UI - Added progressBar which's progress increases according to passed timer percentage over total time

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var counter = 60
+    var timer = Timer()
+    var secondsPassed = 0
+    var totalTime = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        progressBar.progress = 0
+        clearButton.layer.cornerRadius = 6
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        guard let hardness = sender.currentTitle else { return }
+        guard let result = eggTimes[hardness] else { return }
+        
+        totalTime = result
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if secondsPassed <= totalTime {
+            let percentageProgress = Float(secondsPassed)/Float(totalTime)
+            progressBar.progress = percentageProgress
+            
+            secondsPassed += 1
+        } else {
+            titleLabel.text = "DONE!"
+            timer.invalidate()
+        }
+    }
+    
+    
+    @IBAction func clearButtomPressed(_ sender: UIButton) {
+        titleLabel.text = "How do you like your eggs"
+        secondsPassed = 0
+        totalTime = 0
+        progressBar.progress = 0
+        timer.invalidate()
+    }
+    
 }
